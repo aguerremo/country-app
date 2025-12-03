522eaea17298aabf1cf51a23a18dfb8c9e0b6a53
Alfonso <agm0121@alu.medac.es>
2025-12-03 20:31:45 +0100

    Initial commit: Angular Country SPA base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
+import { countryRoutes } from './country/country.routes';
 import { Routes } from '@angular/router';
+import { HomePage } from './shared/pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  },
+
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes'),
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+];
