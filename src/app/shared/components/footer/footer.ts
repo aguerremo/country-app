8a6d798208fbccba3f3bb5600a03d35360842c71
Alfonso <agm0121@alu.medac.es>
2025-12-03 21:25:54 +0100

    feat: add new footer component and update home page UI with hero section and routing.

---
+++ src/app/shared/components/footer/footer.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.html',
+
+})
+export class Footer {
+
+  public currentYear: number = new Date().getFullYear();
+
+ }
--- src/app/shared/pages/home-page/home-page.ts
+++ src/app/shared/pages/home-page/home-page.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.html',
 })
-export class HomePage { }
+export class HomePage {
+
+
+}
